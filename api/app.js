211073106ecda653ed26e8fc09f3f85b987aa5cb
Shashank Agarwal <162678882+shashankagarwalgit@users.noreply.github.com>
2024-06-09 16:57:40 +0530

    Create app.js

---
+++ api/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req,res)=>{
+  const stream = await fs.createReadStream('./100mb.txt', 'utf-8');
+    stream.on('data', (chunk) => {
+        res.write(chunk);
+    });
+    stream.on('end', () => {
+        res.end();
+    });
+
+}
+);
+
+app.get("/hello", (req,res)=>{
+   res.json("Hello world");
+}
+);
+
+app.listen(3000, ()=>{
+    console.log("Server started at 3000");
+});
